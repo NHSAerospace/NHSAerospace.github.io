22ecd1e6b3093e16dc13a0f3c5204ffc233a5c08
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2024-07-05 09:12:07 -0400

    changed awards background

--- src/components/Awards.tsx
+++ src/components/Awards.tsx
@@ -5,13 +5,13 @@ const Awards = () => {
     return (
         <div className='awards'>
             <h1>Awards</h1>
-            <p>Placed 2nd overall in the 2024 Rockets For Schools Competition</p>
-            <p>Placed 1st in Rocket Visuals in the 2024 Rockets For Schools Competition</p>
-            <p>Placed 3rd overall in the 2023 Rockets For Schools Competition</p>
-            <p>Placed 1st in Rocket Visuals in the 2023 Rockets For Schools Competition</p>
-            <p>Placed 2nd in the American Rocketry Challenge 2022 Presentation Competition</p>
-            <p>Placed 1st in the American Rocketry Challenge 2021 Presentation Competition</p>
-            <p>Placed 13th in the American Rocketry Challenge 2020-2021 Nationals</p>
+            <p>2nd overall in the 2024 Rockets For Schools Competition</p>
+            <p>1st in Rocket Visuals in the 2024 Rockets For Schools Competition</p>
+            <p>3rd overall in the 2023 Rockets For Schools Competition</p>
+            <p>1st in Rocket Visuals in the 2023 Rockets For Schools Competition</p>
+            <p>2nd in the American Rocketry Challenge 2022 Presentation Competition</p>
+            <p>1st in the American Rocketry Challenge 2021 Presentation Competition</p>
+            <p>13th in the American Rocketry Challenge 2020-2021 Nationals</p>
         </div>
     );
 }
