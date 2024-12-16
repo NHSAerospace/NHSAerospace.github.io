c162f9dea2f0a505bcbdbabef517b2e2c8fd6e41
mwmcclure7 <mwmcclure7@gmail.com>
2024-12-16 14:54:09 -0500

    Added Evigia and NEF logos

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -3,6 +3,8 @@ import './Sponsors.css';
 import { useState } from 'react';
 import emailjs from 'emailjs-com';
 import microchipLogo from '../images/MICV2CB.png';
+import evigiaLogo from '../images/Evigia_Logo.png';
+import NEFLogo from '../images/NEF_Logo.png';
 
 const Sponsors = () => {
   const [emailStatus, setEmailStatus] = useState('');
@@ -22,9 +24,11 @@ const Sponsors = () => {
     <div className='sponsors'>
       <div className='sponsor-logos'>
         <img src={microchipLogo} alt='Microchip-Logo' className='sponsor-logo' />
+        <img src={evigiaLogo} alt='Evigia-Logo' className='sponsor-logo' />
+        <img src={NEFLogo} alt='NEF-Logo' className='sponsor-logo' />
       </div>
       <h1>Sponsors</h1>
-      <p>To embark on endevours like the NASA Student Launch Initiative and Rockets For Schools, our club depends on funding from our sponsors. All sponsors will have their company logo displayed on our rockets, website, and team T-shirts. Sponsors will also be featured in our NASA SLI proposal. If you would like to help fund our endevours and contribute to the next generation of aerospace engineers, fill out the form below to become a sponsor of the Northville Aerospace Club today!</p>
+      <p>To embark on endeavors like the NASA Student Launch Initiative and Rockets For Schools, our club depends on funding from our sponsors. All sponsors will have their company logo displayed on our rockets, website, and team T-shirts. Sponsors will also be featured in our NASA SLI proposal. If you would like to help fund our endevours and contribute to the next generation of aerospace engineers, fill out the form below to become a sponsor of the Northville Aerospace Club today!</p>
       <form onSubmit={sendEmail} className='sponsor-email'>
         <h1>Become a Sponsor</h1>
         <input type='text' name='user_name' placeholder='Your Name' required />
